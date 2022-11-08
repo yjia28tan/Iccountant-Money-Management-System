c18e7053d502e6869aba3b1802ce82854a0b06c4
germin701 <104176182+germin701@users.noreply.github.com>
2022-11-09 02:42:16 +0800

    Update Forgotpassword-1.py

--- Forgotpassword-1.py
+++ Forgotpassword-1.py
@@ -6,7 +6,9 @@ import sqlite3
 from tkinter import *
 from tkinter import messagebox
 import tkinter as tk
+from PIL import Image, ImageTk
 import customtkinter
+import tkinter.font as tkFont
 
 class ForgotPassword:
 
@@ -21,48 +23,59 @@ class ForgotPassword:
         
         #forgot password frames/pages
         self.fgt_frame = tk.Frame(self.root, bg = 'black')
-        #self.fgt_frame.pack(fill = 'both', expand= 'yes')
-        self.fgt_frame2 = tk.Frame(self.root, bg = 'black')
-        #self.fgt_frame2.pack(fill = 'both', expand= 'yes')
-
-        self.pages =[self.fgt_frame,self.fgt_frame2]
-        self.count = 0
-
-        #page1
-        self.fgt_title = tk.Label(self.fgt_frame, text = 'Reset Password', font = ('Bold', 20, 'bold'),fg = 'white', bg='black')
-        self.fgt_title.pack()
+       
         
-        tk.Label(self.fgt_frame, text =  '', bg= 'black').pack(pady=30)
+        #page1
 
+        self.fgt_title = tk.Label(self.fgt_frame, text = 'Reset Password', fg = 'white', bg='black')
+        self.fgt_title.config(font=tkFont.Font(family='Lato', size=20, weight="bold"))
+        self.fgt_title.pack(anchor = NW, padx=80, pady = 5)
+        Canvas(self.fgt_frame, width=1000, height = 2.0, bg = 'white', highlightthickness = 1).pack(pady=10)
+
+        tk.Label(self.fgt_frame, text =  '', bg= 'black').pack(pady=8)
+
+
+        
          #email
         # User Email Label and Text Entry Box
     
-        self.regemail_lb = tk.Label(self.fgt_frame, text='Email', font=('Bold', 15),fg = 'white', bg='black')
+
+        self.regemail_lb = tk.Label(self.fgt_frame, text='Email',fg = 'white', bg='black')
+        self.regemail_lb.config(font=tkFont.Font(family='Lato', size=15, weight="bold"))
         self.regemail_lb.pack(pady=5)
         self.regemail = StringVar()
-        self.regemail_entry = Entry(self.fgt_frame, width = 30,highlightthickness = 0, textvariable=self.regemail, relief = FLAT, font = ('Bold', 12),fg = 'white', bg = 'black', insertbackground = 'white')
-        self.regemail_entry.pack(pady=5)
+        self.regemail_entry = Entry(self.fgt_frame, justify = 'center',width = 30,highlightthickness = 0, textvariable=self.regemail, relief = FLAT, font = ('Bold', 12),fg = 'white', bg = 'black', insertbackground = 'white')
+        self.regemail_entry.config(font=tkFont.Font(family='Lato', size=12))
+        self.regemail_entry.pack()
         self.regemail_line = Canvas(self.fgt_frame, width=300, height = 2.0, bg = 'white', highlightthickness = 0)
         self.regemail_line.pack()
+        tk.Label(self.fgt_frame, text =  '', bg= 'black').pack(pady=3)
 
 
 
         #newpassword
-        self.newpassword_lb = tk.Label(self.fgt_frame, text='New Password', font=('Bold', 15),fg = 'white', bg='black')
+
+        self.newpassword_lb = tk.Label(self.fgt_frame, text='Password', font=('Bold', 15),fg = 'white', bg='black')
+        self.newpassword_lb.config(font=tkFont.Font(family='Lato', size=15, weight="bold"))
         self.newpassword_lb.pack(pady=5)
         self.newpassword = StringVar()
-        self.newpassword_lb_entry = Entry(self.fgt_frame, width = 30,highlightthickness = 0, textvariable=self.newpassword, relief = FLAT, font = ('Bold', 12),show='*',fg = 'white', bg = 'black', insertbackground = 'white')
-        self.newpassword_lb_entry.pack(pady=5)
+        self.newpassword_lb_entry = Entry(self.fgt_frame, justify = 'center',width = 30,highlightthickness = 0, textvariable=self.newpassword, relief = FLAT, font = ('Bold', 12),show='*',fg = 'white', bg = 'black', insertbackground = 'white')
+        self.newpassword_lb_entry.config(font=tkFont.Font(family='Lato', size=12))
+        self.newpassword_lb_entry.pack()
         self.newpassword_lb_line = Canvas(self.fgt_frame, width=300, height = 2.0, bg = 'white', highlightthickness = 0)
         self.newpassword_lb_line.pack()
+        tk.Label(self.fgt_frame, text =  '', bg= 'black').pack(pady=3)
+
         
      
         #confirm password
-        self.conpassword_lb = tk.Label(self.fgt_frame, text='Confirm New Password', font=('Bold', 15),fg = 'white', bg='black')
+        self.conpassword_lb = tk.Label(self.fgt_frame, text='Confirm Password', fg = 'white', bg='black')
+        self.conpassword_lb.config(font=tkFont.Font(family='Lato', size=15, weight="bold"))
         self.conpassword_lb.pack(pady=5)
         self.conpassword = StringVar()
-        self.conpassword_lb_entry = Entry(self.fgt_frame, width = 30,highlightthickness = 0, textvariable=self.conpassword, relief = FLAT, font = ('Bold', 12),show='*',fg = 'white', bg = 'black', insertbackground = 'white')
-        self.conpassword_lb_entry.pack(pady=5)
+        self.conpassword_lb_entry = Entry(self.fgt_frame,justify = 'center',width = 30, highlightthickness = 0, textvariable=self.conpassword, relief = FLAT, font = ('Bold', 12),show='*',fg = 'white', bg = 'black', insertbackground = 'white')
+        self.conpassword_lb_entry.config(font=tkFont.Font(family='Lato', size=12))
+        self.conpassword_lb_entry.pack()
         self.conpassword_lb_line = Canvas(self.fgt_frame, width=300, height = 2.0, bg = 'white', highlightthickness = 0)
         self.conpassword_lb_line.pack()
 
@@ -75,53 +88,71 @@ class ForgotPassword:
         self.check_btn.pack(pady = 5)
     
 
-        self.fgt_frame.pack(pady = 100)
+        self.fgt_frame.pack(pady = 40)
 
 
-        #page 2
-        self.fgt_title2 = tk.Label(self.fgt_frame2, text = 'Email Verification', font = ('Bold', 20, 'bold'),fg = 'white', bg='black')
-        self.fgt_title2.pack()
-
-        tk.Label(self.fgt_frame2, text =  '', bg= 'black').pack(pady=30)
-                             
-        self.otp_lb = tk.Label(self.fgt_frame2, text='4-digit OTP', font=('Bold', 15),fg = 'white', bg='black')
-        self.otp_lb.pack(pady=5)
-        self.otp_ = StringVar()
-        self.otp_lb_entry = Entry(self.fgt_frame2, width = 30,highlightthickness = 0, textvariable=self.otp_, relief = FLAT, font = ('Bold', 12),show='*',fg = 'white', bg='black', insertbackground = 'white')
-        self.otp_lb_entry.pack()
-        self.otp_lb_line = Canvas(self.fgt_frame2, width=300, height = 2.0, bg = 'white', highlightthickness = 0)
-        self.otp_lb_line.pack()
-                             
-        self.fgt_frame2.pack(pady = 100)
+        
                              
         #buttons
         #next button
         self.sendotpbtn = customtkinter.CTkButton(master=self.fgt_frame,
                                           text="Send/Resend OTP",
-                                          width=90, height=40,
+                                          width=140, height=40,
                                           fg_color="#464E63",
                                           hover_color = "#667190",
                                           command = self.data_validation)
         self.sendotpbtn.pack(pady = 20)
 
+
+        #back button    
+        self.back = ImageTk.PhotoImage(Image.open('backicon.png').resize((40, 40), resample=Image.LANCZOS))
+        self.backbtn1 = Button(self.fgt_frame, image=self.back, bg='black', relief='flat', command=self.move_back_page).place(x=3, y=1)
+
+        
+
+    def page2(self):
+        #pge 2
+        self.fgt_frame2= Toplevel()
+        self.fgt_frame2.geometry("1280x720")
+        self.fgt_frame2.resizable(0, 0)
+        self.fgt_frame2.title("Iccountant")
+        self.fgt_frame2.configure(bg='black')
+
+        
+
+        
+        self.fgt_frame2_title = tk.Label(self.fgt_frame2, text = 'Email Verification', fg = 'white', bg='black')
+        self.fgt_frame2_title.config(font=tkFont.Font(family='Lato', size=20, weight="bold"))
+        self.fgt_frame2_title.place( x=225,y = 45)
+        Canvas(self.fgt_frame2, width=1000, height = 2.0, bg = 'white', highlightthickness = 1).place(x = 640, y=100, anchor =tk.CENTER)
+
+       
+        
+        #otp
+        self.otp_lb = tk.Label(self.fgt_frame2, text='4-digit OTP', fg = 'white', bg='black')
+        self.otp_lb.config(font=tkFont.Font(family='Lato', size=15, weight="bold"))
+        self.otp_lb.place(x = 640,y=230, anchor =tk.CENTER)
+        self.otp_ = StringVar()
+        self.otp_lb_entry = Entry(self.fgt_frame2, justify = 'center',width = 30, highlightthickness = 0, textvariable=self.otp_, relief = FLAT, font = ('Bold', 15),show='*',fg = 'white', bg = 'black', insertbackground = 'white')
+        self.otp_lb_entry.config(font=tkFont.Font(family='Lato', size=12))
+        self.otp_lb_entry.place(x=640,y=265, anchor =tk.CENTER)
+        self.otp_lb_line = Canvas(self.fgt_frame2, width=300, height = 2.0, bg = 'white', highlightthickness = 0)
+        self.otp_lb_line.place(x=640,y=280, anchor =tk.CENTER)
+
         #back button
-        self.backbtn = customtkinter.CTkButton(master=self.fgt_frame2,
-                                          text="Back",
-                                          width=90,
-                                          height=40,
-                                          fg_color="#464E63",
-                                          hover_color = "#667190",
-                                          command = self.move_back_page)
-        self.backbtn.pack(side=tk.LEFT, padx=20, pady=20)
+        self.back2 = ImageTk.PhotoImage(Image.open('backicon.png').resize((40, 40), resample=Image.LANCZOS))
+        self.backbtn2 = Button(self.fgt_frame2, image=self.back2, bg='black', relief='flat', command= lambda: self.fgt_frame2.destroy()).place(x=140, y=40)
+
         #finish session button
         self.finishbtn = customtkinter.CTkButton(master=self.fgt_frame2,
                                           text="Finish",
-                                          width=90, height=40,
+                                          width=140, height=40,
                                           fg_color="#464E63",
                                           hover_color = "#667190",
                                           command = self.otp_validation)
-        self.finishbtn.pack(side = tk.RIGHT,padx = 20)
+        self.finishbtn.pack(side = tk.RIGHT,padx = 140)
 
+        
     def show_password(self):
         if self.btn_value.get() == 1:
             self.newpassword_lb_entry.config(show='')
@@ -136,7 +167,6 @@ class ForgotPassword:
     def data_validation(self):
 
         #email stuff
-        # ?
         self.s = smtplib.SMTP('smtp.gmail.com', 587)
         self.s.starttls()#start TLS for security
         self.s.login("iccountant2022@gmail.com", "vgndqclbhalavixj")#authentication
@@ -176,13 +206,14 @@ class ForgotPassword:
             if self.cursor.fetchone():
                 messagebox.showinfo('Success!', "All of the form is filled!")
                 self.s.sendmail("all2ctt@gmail.com",self.r_email,self.msg)
-                self.move_next_page()
+             
+                self.page2()
             else:
                 messagebox.showerror('Error!', "Please enter the email that is registered in the system!")
         
                 
     def otp_validation(self):
-        print(self.otp_.get())
+        
         if self.otp_.get() == self.OTP:
             messagebox.showinfo('Success!', "OTP verified!")
 
@@ -196,9 +227,7 @@ class ForgotPassword:
             #terminating the session
             self.s.quit()
             self.update_data()
-            #self.move_next_page()
-            print('all good!')
-            #self.new_window()
+            
             
         else:
             messagebox.showerror('Error!', "incorrect OTP try again!")
@@ -211,13 +240,10 @@ class ForgotPassword:
         #open database
         self.conn = sqlite3.connect('Iccountant')
         self.cur = self.conn.cursor()
-        #self.cur.execute("INSERT INTO user VALUES (:name, :username, :email, :password)",
-                    #{'name': self.fname.get(), 'username': self.username.get(), 'email': self.email.get(), 'password': self.password.get()})
-        #self.cur.execute('SELECT email from user where email="%s"'%(self.r_email))
         self.cur.execute("""UPDATE user SET password = ? WHERE email = ?""",(self.npwd,self.r_email))
         
         self.conn.commit()
-        print('should be updated here')
+        
         messagebox.showinfo('Confirmation', 'Record Updated! Session is finished!')
 
     def move_next_page(self):
